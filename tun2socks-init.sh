166bc391ca682883a3ac67500aa5ec056b9fcfa3
jinndi <alncores@gmail.com>
2025-05-17 17:58:02 +0300

    Update tun2socks-init.sh

--- tun2socks-init.sh
+++ tun2socks-init.sh
@@ -8,7 +8,7 @@ SS_IP="${SS_IP:-0.0.0.0}"
 SS_PORT="${SS_PORT:-8388}"
 SS_PASSWORD="${SS_PASSWORD:-your_password}"
 DIF=$(ip route | grep default | awk '{print $5}')
-LIP=$(ip a l $DIF | awk '/inet /{ print $2 }' | cut -f1 -d"/")
+LIP=$(ip a l "$DIF" | awk '/inet /{ print $2 }' | cut -f1 -d"/")
 MIP=$(ip r l | grep "default via" | cut -f3 -d" ")
 
 # Таблица маршрутизации "lip" в /etc/iproute2/rt_tables
@@ -18,19 +18,18 @@ grep -q -E '\s+lip$' /etc/iproute2/rt_tables || echo "20 lip" >> /etc/iproute2/r
 
 # TUN интерфейс
 echo "[tun2socks-init] Setting up $SS_TUN_NAME..."
-ip tuntap add mode tun dev "$SS_TUN_NAME" 2>/dev/null || true
+ip tuntap add mode tun dev "$SS_TUN_NAME" || true 
 ip addr add 192.168.0.33/24 dev "$SS_TUN_NAME"
 ip link set dev "$SS_TUN_NAME" up
-sleep 2
 
 # Маршруты
 echo "[tun2socks-init] Setting up routing...."
-ip route del default dev "$DIF" 2>/dev/null || true
+ip route del default dev "$DIF"
 ip route add default via "$MIP" dev "$DIF" metric 200
 ip rule add from "$LIP" table lip
 ip route add default via "$MIP" dev "$DIF" table lip
 ip route add "$SS_IP/32" via "$MIP" dev "$DIF"
-ip route add default dev "$SS_TUN_NAME" metric 50
+ip route add default dev "$SS_TUN_NAME" metric 50 
 
 # Запуск tun2socks в фоне
 echo "[tun2socks-init] Starting tun2socks..."
@@ -41,4 +40,4 @@ nohup tun2socks \
   > /tmp/tun2socks.log 2>&1 &
 
 echo "[tun2socks-init] Done. Sleeping to allow tun2socks to initialize..."
-sleep 5
+sleep 3

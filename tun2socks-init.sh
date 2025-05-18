6e19083f074c274d29b3b1da7743987b5c907da9
jinndi <alncores@gmail.com>
2025-05-18 11:47:00 +0300

    Update tun2socks-init.sh

--- tun2socks-init.sh
+++ tun2socks-init.sh
@@ -34,7 +34,7 @@ ip route add default dev "$SS_TUN_NAME" metric 50
 # Запуск tun2socks в фоне
 echo "[tun2socks-init] Starting tun2socks..."
 nohup tun2socks -interface "$DIF" -device "tun://$SS_TUN_NAME" \
-  -proxy "ss://chacha20-ietf-poly1305:${SS_PASSWORD}@${SS_IP}:${SS_PORT}" \
+  -proxy "ss://aes-128-gcm:${SS_PASSWORD}@${SS_IP}:${SS_PORT}" \
   > /tmp/tun2socks.log 2>&1 &
 
 echo "[tun2socks-init] Done."

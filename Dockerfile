fd47a040b66d98c4df239e9b28af8a9904625cbc
Jinndi <alncores@gmail.com>
2025-06-29 11:48:08 +0300

    очистка от ненужных комментариев

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,9 @@
 FROM ghcr.io/wg-easy/wg-easy:14
-LABEL maintainer="WG-EASY-BREEZY"
+LABEL org.opencontainers.image.title="wg-easy-breezy" \
+    org.opencontainers.image.description="wg-easy v14 mod" \
+    org.opencontainers.image.authors="Jinndi <alncores@gmail.com>" \
+    org.opencontainers.image.version="4"
 
-# Используемая версия tun2socks
 # https://github.com/xjasonlyu/tun2socks/releases
 ARG TUN2SOCKS_RELEASE="v2.6.0"
 
@@ -10,24 +12,17 @@ RUN apk --update upgrade --no-cache
 
 WORKDIR /app
 
-# Загрузка tun2socks
 RUN curl -L https://github.com/xjasonlyu/tun2socks/releases/download/${TUN2SOCKS_RELEASE}/tun2socks-linux-amd64.zip > tun2socks-linux-amd64.zip && \
     unzip tun2socks-linux-amd64.zip && \
     mv tun2socks-linux-amd64 tun2socks && \
     chmod a+x tun2socks
 
-# Добавлении таблицы маршрутизации "lip" в /etc/iproute2/rt_tables
 RUN mkdir -p /etc/iproute2 && echo "20 lip" >> /etc/iproute2/rt_tables
 
-# Копирование конфига настройки сети sysctl.conf
 COPY ./sysctl.conf /etc/sysctl.conf
 
-# Копирование entrypoint.sh скрипта
 COPY ./start.sh start.sh
 RUN chmod a+x start.sh
 
-# Устанавливаем dumb-init как init-процесс (PID 1)
 ENTRYPOINT ["/usr/bin/dumb-init", "--"]
-
-# Команда при запуске контейнера
 CMD ["/app/start.sh"]
--- start.sh
+++ start.sh
@@ -2,47 +2,30 @@
 
 set -e
 
-# Загрузка модулей tcp_congestion_control
 for module in tcp_hybla tcp_bbr; do
   if modprobe -q "$module"; then
-    echo "[start.sh] Модуль $module загружен"
+    echo "[start.sh] Module $module loaded"
   else
-    echo "[start.sh] Ошибка загрузки модуля $module"
+    echo "[start.sh] Module loading error $module"
   fi
 done
 
-# Применяем сетевые настройки из /etc/sysctl.conf
 /sbin/sysctl -p > /dev/null 2>&1
 
-# Если SS_LINK (Ссылка SIP002 URI scheme для полкючение к серверу shadowsocks) не пуста
 if [ -n "$SS_LINK" ]; then
-
-  # Добавляем префикс ss:// к ссылки подключения, если его нет
   [[ "$SS_LINK" != ss://* ]] && SS_LINK="ss://${SS_LINK}"
-
-  # Название для прокси интерфейса shadowsocks
   SS_TUN_NAME="${SS_TUN_NAME:-tun0}"
-
-  # Извлечение IP сервера из ссылки ss://...@IP:port
   SS_IP=$(echo "$SS_LINK" | awk -F'[@:]' '{print $(NF-1)}')
-
-  # Получение имени интерфейса по умолчанию
   DIF=$(ip route | awk '/default/ {print $5}' | head -n1)
-
-  # Получение локального IP адреса на интерфейсе
   LIP=$(ip -4 addr show "$DIF" | awk '/inet / {print $2}' | cut -d/ -f1)
-
-  # Получение основного шлюза
   MIP=$(ip route | awk '/default via/ {print $3}' | head -n1)
 
-  # Настройка и поднятие TUN интерфейса
-  echo "[start.sh] Настраиваем $SS_TUN_NAME интерфейс..."
+  echo "[start.sh] Configure $SS_TUN_NAME interface..."
   ip tuntap add dev "$SS_TUN_NAME" mode tun 2>/dev/null || true
   ip addr add 192.168.0.33/24 dev "$SS_TUN_NAME"
   ip link set "$SS_TUN_NAME" up
 
-  # Настройка маршрутов
-  echo "[start.sh] Настраиваем ip routing...."
+  echo "[start.sh] Configure ip routing...."
   ip route del default dev "$DIF"
   ip route add default via "$MIP" dev "$DIF" metric 200
   ip rule add from "$LIP" table lip
@@ -50,12 +33,10 @@ if [ -n "$SS_LINK" ]; then
   ip route add "$SS_IP/32" via "$MIP" dev "$DIF"
   ip route add default dev "$SS_TUN_NAME" metric 50 
 
-  # Запускаем прокси shadowsocks через tun2socks
-  echo "[start.sh] Запускаем tun2socks proxy к $SS_IP..."
+  echo "[start.sh] Launch tun2socks proxy to $SS_IP..."
   nohup /app/tun2socks -proxy "$SS_LINK" -interface "$DIF" -device "tun://$SS_TUN_NAME" \
     -tcp-rcvbuf 1048576 -tcp-sndbuf 1048576 -loglevel "error" > /app/tun2socks.log 2>&1 &
 fi
 
-# Запуск сервера wg-easy
-echo "[start.sh] Запускаем WEB UI сервер wg-easy..."
+echo "[start.sh] Launch WEB UI server wg-easy..."
 exec node /app/server.js
